a63cd5a20db0c7f44eb9e8a2ea8e05c7f4d422c9
Tony Yip <tony.cnyip@gmail.com>
2020-04-10 13:14:29 +0800

    Make this work

---
+++ script.js
@@ -0,0 +1,29 @@
+	var fileSelector = document.getElementById("file_input");
+	fileSelector.addEventListener('change', ()=>{
+        var file = fileSelector.files[0];
+		fname = file.name;
+		var reader = new FileReader();
+		reader.onload = function (e) {
+			var contents = e.target.result;
+			prepareText(contents);
+		};
+		reader.readAsText(file);
+    });
+
+function prepareText(inputString) {
+    var paragraphs = inputString.split(/[\r\n]+/)
+    
+    showText(paragraphs);
+}
+
+function showText(paragraphs){
+    var page = document.getElementById("text")
+    page.innerHTML = "";
+    var html = "";
+    for (let index = 0; index < paragraphs.length; index++) {
+        //TODO parse styling
+        //TODO convert notations, like "…", "“", "—"
+        html += "<p>" + paragraphs[index] + "</p>";
+    }
+    page.innerHTML = html;
+}
